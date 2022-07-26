c7f7e1b52c824cdfd98553e717b1e8031e0fc709
DimaHalavach <gekfin91@gmail.com>
2022-07-26 18:27:04 +0300

    fix: fix slider

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -20,19 +20,30 @@ const burger_menu_closed = document.querySelector('.burger_menu');
 
 let offset = 0;
 const sliderLine = document.querySelector('.destinations_img');
+const mediaSliderLine = document.querySelector('.media_img_destinations');
 const spain = document.querySelector('.img_spain');
+const usa = document.querySelector('.img_usa');
+const arrowRight = document.querySelector('.arrow_right');
 document.querySelector('.img_usa').addEventListener('click', function(){
-    offset = offset + 799.99;
+    offset = offset + 819.99;
     if (offset > 1440) {
         offset = 0;
     }
     sliderLine.style.left = -offset + 'px';
-    spain.style.right = 800 + 'px';
- 
+     
+})
+document.querySelector('.arrow_right').addEventListener('click', function(){
+    offset = offset + 360;
+    if (offset > 1080) {
+        offset = 0;
+    }
+    mediaSliderLine.style.left = -offset + 'px';
+     
 })
 
+
 document.querySelector('.img_spain').addEventListener('click', function(){
-    offset = offset + 799.99;
+    offset = offset + 819.99;
     if (offset > 1440) {
         offset = 0;
     }
